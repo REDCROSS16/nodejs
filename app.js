21e220f2f3fd557c9bfb8c49c7751b09570f6cab
redcross16 <belkill@mail.ru>
2020-10-23 00:04:21 +0300

    testing function

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log('hello world');
+
+
+let my = 'Alex';
+
+function name(name) {
+    console.log(`${name} Добрый вечер!`); 
+}
+
+name(my);
+
+let user = {
+    name: 'RED',
+    age: '27',
+    read() {
+        console.log(`${this.name} read`);
+        if  (this.name == 'RED') {
+            sayHi();
+        }
+    }
+ } 
+function sayHi() {
+    console.log('hi');
+}
+ user.read();
+
+ admin = user;
+ admin.name = 'YURA';
+ admin.read();
+
+ user.read();
