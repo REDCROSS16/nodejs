cff965516b339f16a644e7ffc5277ed706ca1156
redcross16 <belkill@mail.ru>
2020-11-02 22:53:03 +0300

    add express to project and gitignore

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
+const express = require('express');
+
 console.log('hello world');
 
 
-let my = 'Alex';
-
-function name(name) {
-    console.log(`${name} Добрый вечер!`); 
-}
-
-name(my);
-
-let user = {
-    name: 'RED',
-    age: '27',
-    read() {
-        console.log(`${this.name} read`);
-        if  (this.name == 'RED') {
-            sayHi();
-        }
-    }
- } 
-function sayHi() {
-    console.log('hi');
-}
- user.read();
-
- admin = user;
- admin.name = 'YURA';
- admin.read();
-
- user.read();
