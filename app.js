2c38f1831e5a9051c78d2c4cf46c1e160ff3d954
redcross16 <belkill@mail.ru>
2020-11-02 23:07:11 +0300

    test app.get

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const app = express();
 
-console.log('hello world');
+app.get('/', (req, res) => {
+    res.send('hello world')
+});
 
+app.get('/json', (req, res) => {
+    res.status(200).json({
+        message: 'Hello',
+    });
+});
 
+app.listen(3000, ()=> {
+    console.log('Example to listen posrt 3000');
+});
